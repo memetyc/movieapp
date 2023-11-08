c4f88bd9825abc8d17c8b6ae5c1ce8e5c63e5f24
Mehmet Yusuf Çalışkan <119367838+memetyc@users.noreply.github.com>
2023-11-08 11:00:44 +0300

    Update settings.py

--- movieapp/settings.py
+++ movieapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@_6(x$(-aym%@7xyyw15icwtjo7_rip5w!@py@w#vphmmy9go-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
